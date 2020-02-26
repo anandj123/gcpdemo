7a630404f4dabe52697d41083ad219c3acf79dc8
Anand Jain <jain.12@gmail.com>
2020-02-26 17:36:53 -0500

    week 3 - 2

--- week3/create_table.sql
+++ week3/create_table.sql
@@ -1,3 +1,13 @@
-SELECT 
-    COUNT(*) 
-FROM `bigquery-public-data`.samples.shakespeare
+ CREATE TABLE Anand_BQ_Test_1.newtable
+ (
+   x INT64 OPTIONS(description="An optional INTEGER field"),
+   y STRUCT<
+     a ARRAY<STRING> OPTIONS(description="A repeated STRING field"),
+     b BOOL
+   >
+ )
+ OPTIONS(
+   expiration_timestamp=TIMESTAMP "2023-01-01 00:00:00 UTC",
+   description="a table that expires in 2023",
+   labels=[("org_unit", "development")]
+ )
--- week3/deploy.py
+++ week3/deploy.py
@@ -1,5 +1,46 @@
+import os, sys, getopt
+import logging
 import os
+from google.cloud import storage
 
-with open('create_table.sql', 'r') as f:
-    sql = f.read()
-    os.system("bq query --nouse_legacy_sql '" + sql + "'")
+# use service account
+#export GOOGLE_APPLICATION_CREDENTIALS="/home/user/Downloads/[FILE_NAME].json"
+def read_file(filename):
+    print('Reading the full file contents:\n')
+    # Instantiates a client
+    storage_client = storage.Client()
+
+    bucket = storage_client.get_bucket('anand-bq-test-2')
+    blob = storage.Blob('dataflow/functions/test.js', bucket)
+    with open('./test.js', 'w') as file_obj:
+        file_obj.write(blob.download_as_string())
+        file_obj.close()
+        
+
+    
+
+def main(argv):
+   inputfile = ''
+   outputfile = ''
+   try:
+      opts, args = getopt.getopt(argv,"hi:",["ifile="])
+   except getopt.GetoptError:
+      print 'deploy.py -i <inputfile> '
+      sys.exit(2)
+   for opt, arg in opts:
+      if opt == '-h':
+         print 'deploy.py -i <inputfile> '
+         sys.exit()
+      elif opt in ("-i", "--ifile"):
+         inputfile = arg
+      
+   print 'Input file is :', inputfile
+   read_file(inputfile)
+
+def create_bq_table():   
+    with open('create_table.sql', 'r') as f:
+        sql = f.read()
+        os.system("bq query --nouse_legacy_sql '" + sql + "'")
+
+if __name__ == "__main__":
+   main(sys.argv[1:])
